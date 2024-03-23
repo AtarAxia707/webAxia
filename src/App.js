e2c2eaee28a6a3952bd3f3e522bf475be461d249
AtarAxia707 <alan910707@gmail.com>
2024-03-23 21:15:24 +0800

    0323 commit

--- src/App.js
+++ src/App.js
@@ -7,10 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          axia no web desu
+          axia no web desu Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
