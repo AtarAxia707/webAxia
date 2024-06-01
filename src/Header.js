0d0110bc285bee1b7a4d40d1c0beff1504b375a8
AtarAxia707 <alan910707@gmail.com>
2024-06-01 21:55:38 +0800

    0601 commit add rolling wise picture

---
+++ src/Header.js
@@ -0,0 +1,25 @@
+import { MenuItem,Menu,Icon } from 'semantic-ui-react';
+//target='_blank' open a new tab
+function Header(){
+  return(
+    <Menu size='large' inverted>
+      <MenuItem href='http://facebook.com' target='_blank'>
+        <Icon name='facebook' size='large'/>
+        FaceBook
+      </MenuItem>
+      
+      <MenuItem href='http://YouTube.com' target='_blank'>
+      <Icon name='youtube' size='large'/>
+        YouTube
+      </MenuItem>
+
+      <MenuItem href='http://instagram.com' target='_blank'>
+        <Icon name='instagram' size='large'/>
+        Instagram
+      </MenuItem>
+    </Menu>
+  )
+  
+}
+
+export default Header;
